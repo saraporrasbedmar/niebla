a0f9e268b486a7f3176d2fbd69f0581aad3bba50
aras <saraporrasbedmar@gmail.com>
2025-08-25 15:09:09 +0200

    working stage

--- setup.py
+++ setup.py
@@ -125,8 +125,9 @@ setup(
     #},
     # If there are data files included in your packages that need to be
     # installed, specify them here.
+    include_package_data=True,
     package_data={  # Optional
-        "niebla": ["src/niebla/data/*"],
+        "niebla": ["data/*"],
     },
     # Entry points. The following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
